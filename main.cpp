67493d916732e2608e2367ff9b9cf294a7adddba
polinb <PolinB2413@gmail.com>
2019-12-28 13:30:08 +0300

    Catch exception in main. Added requests to client.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
-#include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <zconf.h>
-#include <vector>
-#include <poll.h>
-#include "Server.h"
 #include "EPollCoordinator.h"
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
 int main() {
-    EPollCoordinator ePoll(1234);
-    ePoll.start();
+    try {
+        EPollCoordinator ePoll(1234);
+        ePoll.start();
+    } catch (ServerException& e) {
+        std::cerr << e.what();
+    }
     return 0;
 }
-#pragma clang diagnostic pop
